b7c0ceb2e909883ee9605516ae4d786fc372f328
Igor Cherepanov <zigorewslike@gmail.com>
2023-03-31 01:48:36 +0300

    Abstracting OpenGL (renderer)

--- TheChernoGL/src/Application.cpp
+++ TheChernoGL/src/Application.cpp
@@ -7,7 +7,9 @@
 #include <sstream>
 
 #include "Renderer.h"
+
 #include "VertexBuffer.h"
+#include "VertexBufferLayout.h"
 #include "IndexBuffer.h"
 #include "VertexArray.h"
 #include "Shader.h"
@@ -74,19 +76,18 @@ int main(void)
         ib.Unbind();
         shader.Unbind();
 
+        Renderer renderer;
+
         /* Loop until the user closes the window */
         while (!glfwWindowShouldClose(window))
         {
             /* Render here */
-            glClear(GL_COLOR_BUFFER_BIT);
+            renderer.Clear();
 
             shader.Bind();
             shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
 
-            va.Bind();
-            ib.Bind();
-
-            GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+            renderer.Draw(va, ib, shader);
 
             if (r > 1.0f) {
                 increment = -0.01f;
