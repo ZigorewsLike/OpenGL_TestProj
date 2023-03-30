5f8faf9aa7017aa47be16a1f198f0cb339a3162b
Igor Cherepanov <zigorewslike@gmail.com>
2023-03-31 01:05:41 +0300

    Abstracting OpenGL (shader)

--- TheChernoGL/src/Application.cpp
+++ TheChernoGL/src/Application.cpp
@@ -10,83 +10,8 @@
 #include "VertexBuffer.h"
 #include "IndexBuffer.h"
 #include "VertexArray.h"
+#include "Shader.h"
 
-enum class ShaderType {
-    NONE = -1, 
-    VERTEX = 0,
-    FRAGMENT = 1
-};
-
-struct ShaderProgramSource {
-    std::string vertexSource;
-    std::string fragmentSource;
-};
-
-static ShaderProgramSource ParseShader(const std::string& filepath) {
-    std::ifstream stream(filepath);
-
-    std::string line;
-    std::stringstream ss[2];
-    ShaderType type = ShaderType::NONE;
-    while (getline(stream, line)) {
-        if (line.find("shader") != std::string::npos) {
-            if (line.find("vertex") != std::string::npos) {
-                type = ShaderType::VERTEX;
-            }
-            else if (line.find("fragment") != std::string::npos)
-            {
-                type = ShaderType::FRAGMENT;
-
-            }
-        }
-        else {
-            ss[(int)type] << line << "\n";
-        }
-    }
-    return { ss[0].str(), ss[1].str() };
-}
-
-static unsigned int CompileShader(unsigned int type, const std::string& source) {
-    unsigned int id = glCreateShader(type);
-    const char* src = source.c_str();
-    glShaderSource(id, 1, &src, nullptr);
-    glCompileShader(id);
-
-    int result;
-    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
-
-    if (result == GL_FALSE) { 
-        int length;
-        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
-        char* message = (char*)malloc(length * sizeof(char));
-        glGetShaderInfoLog(id, length, &length, message);
-
-        std::cout << "Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << std::endl;
-        std::cout << message << std::endl;
-        glDeleteShader(id);
-
-        return 0;
-    }
-
-    return id;
-}
-
-static int CreateShader(const std::string& vertexShader, const std::string& fragmenyShader) {
-    unsigned int program = glCreateProgram();
-    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
-    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmenyShader);
-
-    glAttachShader(program, vs);
-    glAttachShader(program, fs);
-    glLinkProgram(program);
-    glValidateProgram(program);
-
-    glDeleteShader(vs);
-    glDeleteShader(fs);
-
-    return program;
-
-}
 
 int main(void)
 {
@@ -137,21 +62,17 @@ int main(void)
 
         IndexBuffer ib(indices, 6);
 
-        ShaderProgramSource shaderSource = ParseShader("res/shaders/Basic.shader");
-        unsigned int shader = CreateShader(shaderSource.vertexSource, shaderSource.fragmentSource);
-        GLCall(glUseProgram(shader));
-
-        int location = glGetUniformLocation(shader, "u_Color");
-        ASSERT(location != -1);
-        GLCall(glUniform4f(location, 0.2f, 0.3f, 0.8f, 1.0f));
+        Shader shader("res/shaders/Basic.shader");
+        shader.Bind();
+        shader.SetUniform4f("u_Color", 0.2f, 0.3f, 0.8f, 1.0f);
 
         float r = 0.0f;
         float increment = 0.01f;
         
         va.Unbind();
-        GLCall(glUseProgram(0));
-        GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
-        GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
+        vb.Unbind();
+        ib.Unbind();
+        shader.Unbind();
 
         /* Loop until the user closes the window */
         while (!glfwWindowShouldClose(window))
@@ -159,8 +80,8 @@ int main(void)
             /* Render here */
             glClear(GL_COLOR_BUFFER_BIT);
 
-            GLCall(glUseProgram(shader));
-            GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
+            shader.Bind();
+            shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
 
             va.Bind();
             ib.Bind();
@@ -181,7 +102,6 @@ int main(void)
             /* Poll for and process events */
             glfwPollEvents();
         }
-        GLCall(glDeleteProgram(shader));
     }
 
     glfwTerminate();
---
+++ TheChernoGL/src/Shader.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <string>
+#include <unordered_map>
+
+struct ShaderProgramSource {
+	std::string vertexSource;
+	std::string fragmentSource;
+};
+
+class Shader {
+private:
+	unsigned int m_RenderID;
+	std::string m_FilePath;
+	std::unordered_map<std::string, int> m_UniformLocationCache;
+
+public:
+	Shader(const std::string& filename);
+	~Shader();
+
+	void Bind() const;
+	void Unbind() const;
+
+	// Set uniforms
+	void SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3);
+	
+private:
+	ShaderProgramSource ParseShader(const std::string& filepath);
+	unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmenyShader);
+	unsigned int CompileShader(unsigned int type, const std::string& source);
+
+	int GetUniformLocation(const std::string& name);
+};
